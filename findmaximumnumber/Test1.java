a30b19ba17dda16b9d1dd67d0fed52923df98922
priyanka897-star <priyankamore8997@gmail.com>
2021-10-13 14:07:27 +0530

    Added Test case

---
+++ findmaximumnumber/Test1.java
@@ -0,0 +1,24 @@
+package findmaximumnumber;
+
+import org.junit.Test;
+
+class Test1 {
+
+	@Test
+	public static Integer findMax(Integer x, Integer y, Integer z) {
+		Integer max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+
+		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Maximum Number:" + findMax(23, 20, 21));
+
+	}
+}
---
+++ findmaximumnumber/Test2.java
@@ -0,0 +1,25 @@
+package findmaximumnumber;
+
+import org.junit.Test;
+
+class Test2 {
+
+	@Test
+	public static Integer findMax(Integer x, Integer y, Integer z) {
+		Integer max = y;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
+		}
+
+		return max;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Maximum Number:" + findMax(45, 67, 72));
+
+	}
+
+}
